dd24fd278db39c4812be6552da5ebe59c7b4a99e
israel20162 <israelpadonu14@gmail.com>
2022-06-20 20:04:13 +0100

    .

--- app/Views/get_involved.php
+++ app/Views/get_involved.php
@@ -1,77 +1,90 @@
-<section>
+<!DOCTYPE html>
+<html lang="en">
 
-    <div class="d-grid text-center container ">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <title>Document</title> -->
+</head>
 
-        <h2 class="text-capitalize"> how it works</h2>
+<body>
 
 
-        <p class="col-lg-6" style="justify-self: center;">To ensure integrity, we're there through every step of every project.
-            We vet all requests, purchase each item, and ship
-            materials directly to verified teachers.</p>
+    <section>
+
+        <div class="d-grid text-center container ">
+
+            <h2 class="text-capitalize"> how it works</h2>
 
 
-        <div class="d-flex gap-3 text-center hiw" style="justify-self: center;justify-content:space-between;flex-direction: row;">
+            <p class="col-lg-6" style="justify-self: center;">To ensure integrity, we're there through every step of every project.
+                We vet all requests, purchase each item, and ship
+                materials directly to verified teachers.</p>
 
-            <div class="col-lg-4 ">
-                <h1 class="image-1 how-it-image "></h1>
-                <span style="font-size: 21px;font-weight:600" class="my-lg-5">You find a project</span>
-                <p class=" ">Find a classroom project that inspires you and give as little as $1.
-                    We're a public charity, so your donation is
-                    tax-deductible.</p>
+
+            <div class="d-flex gap-3 text-center hiw" style="justify-self: center;justify-content:space-between;flex-direction: row;">
+
+                <div class="col-lg-4 ">
+                    <h1 class="image-1 how-it-image "></h1>
+                    <span style="font-size: 21px;font-weight:600" class="my-lg-5">You find a project</span>
+                    <p class=" ">Find a classroom project that inspires you and give as little as $1.
+                        We're a public charity, so your donation is
+                        tax-deductible.</p>
+                </div>
+
+                <div class="col-lg-4">
+                    <h1 class="how-it-image image-2 mx-lg-5 "></h1>
+                    <span style="font-size: 21px;font-weight:600;margin:110px auto" class="my-5">We fulfill the order</span>
+                    <p class="w-100 ">Find a classroom project that inspires you and give as little as $1.
+                        We're a public charity, so your donation is
+                        tax-deductible.</p>
+                </div>
+                <div class="col-lg-4">
+                    <h1 class="how-it-image image-3"></h1>
+                    <span style="font-size: 21px;font-weight:600" class="my-lg-5">Teachers thank you</span>
+                    <p class="w-100 ">Find a classroom project that inspires you and give as little as $1.
+                        We're a public charity, so your donation is
+                        tax-deductible.</p>
+                </div>
+
             </div>
 
-            <div class="col-lg-4">
-                <h1 class="how-it-image image-2 mx-lg-5 "></h1>
-                <span style="font-size: 21px;font-weight:600;margin:110px auto" class="my-5">We fulfill the order</span>
-                <p class="w-100 ">Find a classroom project that inspires you and give as little as $1.
-                    We're a public charity, so your donation is
-                    tax-deductible.</p>
-            </div>
-            <div class="col-lg-4">
-                <h1 class="how-it-image image-3"></h1>
-                <span style="font-size: 21px;font-weight:600" class="my-lg-5">Teachers thank you</span>
-                <p class="w-100 ">Find a classroom project that inspires you and give as little as $1.
-                    We're a public charity, so your donation is
-                    tax-deductible.</p>
-            </div>
+
+
 
         </div>
 
 
 
 
-    </div>
+    </section>
 
 
 
 
-</section>
+    <section class="tap">
+
+        <div class="conatiner tap-container">
+
+            <h2 style="color: #ffc80b;">Get involved</h2>
+
+            <h5 class="w-75 my-lg-4 my-4" style="color:#ffc80b">
+
+                As a teacher-founded nonprofit, we're trusted by teachers, donors,
+                administrators, and partner organizations across thecountry.
+
+            </h5>
+
+
+            <div style="display: flex;flex-direction:row;justify-items:center;justify-content:space-between">
+
+                <button class="btn border-1 border-light btn-outline "><a href="http://" class="text-light" style="text-decoration:none ;">support teachers monthly</a></button>
+                <button class="btn border-1 border-light btn-outline mx-4"><a href="http://" class="text-light" style="text-decoration:none ;">support teachers monthly</a></button>
+                <button class="btn border-1 border-light btn-outline "><a href="http://" class="text-light" style="text-decoration:none ;">support teachers monthly</a></button>
+            </div>
 
 
 
-
-<section class="tap">
-
-    <div class="conatiner tap-container">
-
-        <h2 style="color: #ffc80b;">Get involved</h2>
-
-        <h5 class="w-75 my-lg-4 my-4" style="color:#ffc80b">
-
-            As a teacher-founded nonprofit, we're trusted by teachers, donors,
-            administrators, and partner organizations across thecountry.
-
-        </h5>
-
-
-    <div style="display: flex;flex-direction:row;justify-items:center;justify-content:space-between">
-
-        <button class="btn border-1 border-light btn-outline "><a href="http://" class="text-light" style="text-decoration:none ;">support teachers monthly</a></button>
-        <button class="btn border-1 border-light btn-outline mx-4"><a href="http://" class="text-light" style="text-decoration:none ;">support teachers monthly</a></button>
-        <button class="btn border-1 border-light btn-outline "><a href="http://" class="text-light" style="text-decoration:none ;">support teachers monthly</a></button>
-</div>
-
-
-
-    </div>
-</section>
+        </div>
+    </section>
