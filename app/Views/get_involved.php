debd18de33e17c03d4347ce43e20159b47eb1892
israel <israelpadonu14@gmail.com>
2024-05-24 23:31:06 +0100

    last

--- app/Views/get_involved.php
+++ app/Views/get_involved.php
@@ -83,9 +83,33 @@
                 <button class="btn border-1 border-light btn-outline "><a href="http://" class="text-light" style="text-decoration:none ;">support teachers monthly</a></button>
                 <!-- <button class="btn border-1 border-light btn-outline mx-4"><a href="http://" class="text-light" style="text-decoration:none ;">support teachers monthly</a></button>
                 <button class="btn border-1 border-light btn-outline "><a href="http://" class="text-light" style="text-decoration:none ;">support teachers monthly</a></button> -->
-            </div> 
+            </div>
 
 
 
         </div>
+
+        <img src="https://view.genial.ly/62b44658a0f4320018b2315c/interactive-image-interactive-image" alt="" srcset="">
+
+
+
+        <div class="container-wrapper-genially" style="position: relative; min-height: 400px; width: 70%;"><video class="loader-genially" autoplay="autoplay" loop="loop" playsinline="playsInline" muted="muted" style="position: absolute;top: 45%;left: 50%;transform: translate(-50%, -50%);width: 80px;height: 80px;margin-bottom: 10%">
+                <source src="https://static.genial.ly/resources/panel-loader-low.mp4" type="video/mp4" />Your browser does not support the video tag.
+            </video>
+            <div id="62b44658a0f4320018b2315c" class="genially-embed" style="margin: 0px auto; position: relative; height: auto; width: 100%;"></div>
+        </div>
+        <script>
+            (function(d) {
+                var js, id = "genially-embed-js",
+                    ref = d.getElementsByTagName("script")[0];
+                if (d.getElementById(id)) {
+                    return;
+                }
+                js = d.createElement("script");
+                js.id = id;
+                js.async = true;
+                js.src = "https://view.genial.ly/static/embed/embed.js";
+                ref.parentNode.insertBefore(js, ref);
+            }(document));
+        </script>
     </section>
